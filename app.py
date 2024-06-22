a089b8be48a6178c91c153835f4706475a494caf
Muhammad Suleman <leodeveloper@gmail.com>
2024-06-22 16:52:47 +0400

    basic structure done

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from src.utils_method import get_query_params
+
+
+
+# Main function to run the app
+def main():
+    st.title("Query String Example")
+    query_params = get_query_params()
+    
+    if query_params:
+        st.write("Query parameters:")
+        for key, value in query_params.items():
+            st.write(f"{key}: {value}")
+    else:
+        st.write("No query parameters found.")
+
+if __name__ == "__main__":
+    main()
